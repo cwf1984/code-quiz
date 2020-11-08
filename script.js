df9665bcf75c97f2bc10f59cfa7abbbbe3ce24fc
Vanessa Malloy <catwoman.fan.84@gmail.com>
2020-11-08 13:22:57 -0800

    new questions, for loop started

--- script.js
+++ script.js
@@ -1,47 +1,91 @@
 //on load, Welcome-Page appears
 //question-container is not visible
-// WHEN click start button, timer starts and decrements by 1 seconds
-
-var timerCount = document.getElementById("timer");
-var startButton = document.getElementById("startButton");
-var secondsLeft = 75;
-var qtnContainter = document.getElementById("question-container");
-var welcomePg = document.getElementById("Welcome-Page");
-var answerBtn = document.querySelectorAll("answerbtn");
-var questions = [
-
-    {question: "How do we turn a string into numbers?",
-     answers: ["isNaN()",
-                 "toLocaleString()", 
-                 "parseInt()", 
-                 "valueOf()"],
-     correct: 2},
-
-    {question: "How do we create a p tag in JavaScript?",
-     answers: ["document.createElement('p')", 
-                 "document.setAttribute('p')", 
-                 "document.getElementById('p')", 
-                 "document.querySelector('p')"],
-     correct: 0},
-
-    {question: "What does event.stopPropogation() do?",
-     answers: ["Stops the current math function from running", 
-                 "Stops a timer from running past 0", 
-                 "Stops an event from continuing to happen in a parent or child element", 
-                 "Ends the current event that is running"],
-     correct: 2},
-
-    {question: "What is document.querySelector() used for?",
-     answers: ["Returns the element with the class name specified", 
-                 "Returns the first element that matches the CSS selector",
-                 "Returns the element with the tag name specified", 
-                 "Returns the element with ID name specified"],
-     correct : 1}
-];
 
 window.onload = function() {
     qtnContainter.setAttribute("class", "hide");
   }
+// WHEN click start button, timer starts and decrements by 1 seconds
+
+var timerCount = document.getElementById("timer");
+var startButton = document.getElementById("startButton");
+var secondsLeft = 150;
+var qtnContainter = document.getElementById("question-container");
+var welcomePg = document.getElementById("Welcome-Page");
+var answerButton = document.getElementsByClassName("answerbtn");
+var score = 150;
+
+questions = [
+    {question: "Seredipity",
+     answers: ["The positive occurence and development of events by change",
+               "Existing or being everywhere at the same time", 
+               "To find and bring to light", 
+               "A calm mind that is free from agitation"],
+     correct: 0},
+
+     {question:"Petrichor",
+     answers: ["A type of intertebrate found only in caves in the Mediterranean  Sea",
+               "A type of moss", 
+               "The sound of dry leaves rustling in the wind", 
+               "The scent produced by the first rain after a period of warm, dry weather."],
+     correct: 3},
+
+     {question: "Effervescent",
+     answers: ["Flat",
+               "Fizzy", 
+               "Soothing", 
+               "Medicinal"],
+     correct: 1},
+
+     {question: "Ephemeral",
+     answers: ["Spiritual",
+               "Of a similuar nature or character", 
+               "Principal source", 
+               "Lasting for a very short time"],
+     correct: 3},
+
+     {question: "Borborigmus",
+     answers: ["Surgical removal of body tissue",
+               "Erosion of rock due to strong winds", 
+               "Intestinal Rumbling", 
+               "A chemical agent that interferes with another substance"],
+     correct: 2},
+
+     {question: "Neutropenia",
+     answers: ["Very low levels of a type of white blood cell.",
+               "A clotting disorder where are platelet levels are dangerously low", 
+               "B12 deficiency which is caused by a weakened stomach lining or autoimmune condition", 
+               "White blood cells drastically decline due to a parasitic infection from a mosquito bite"],
+     correct: 0},
+
+     {question: "Bemused",
+     answers: ["Entertained",
+               "Puzzled", 
+               "Enlightened", 
+               "Confused"],
+     correct: 1},
+
+     {question: "Dichotomy",
+     answers: ["Division into two parts",
+               "Requiring continual and often ineffective effort", 
+               "An insubstantial form", 
+               "To act as a decoy"],
+     correct: 0},
+
+     {question: "Erudite",
+     answers: ["Having or occuring in great variety",
+               "Having or showing knowledge that is gained by studying", 
+               "To feel penitence, remorse or regret for", 
+               "To offer as an example, reason or proof in a discussion or analyis"],
+     correct: 1},
+
+     {question: "Encumber",
+     answers: ["Sideways",
+               "Unimaginative", 
+               "Burden", 
+               "Scrupulous"],
+     correct: 2}
+]
+
 
 // Countdown Timer starts of click of Start Button
 
@@ -61,109 +105,35 @@ function timeRemaining() {
 startButton.addEventListener("click", timeRemaining);
 startButton.addEventListener("click", startGame);
 
+// WHEN click start button
+
+function questionStart( ) {
+    if (welcomePage.style.display==="none") {
+        questionContainer.style.display = "block";
+    }
+    else {
+        questionContainer.style.display = "none";
+    }};
+
 function startGame () {
     welcomePg.setAttribute("class", "hide");
     startButton.setAttribute("class", "hide");
-    qtnContainter.setAttribute("class", "visible")
-    //set attribute hide to Welcome Page and start button
-   //click start button
-   //welcome page hides 
-   //question container appears with question and answers in buttons
-       qtn1();
-    //onClick qtn2 function runs
-    // answerBtn.addEventListener("click", qtn2);
-    //function of next questions
-    //call the function
-}
+    qtnContainter.setAttribute("class", "visible");
 
-function qtn1() {
-var questionText = document.getElementById("question");
-questionText.textContent = questions[0].question;
-var answerChoice1 = document.getElementById("button1");
-answerChoice1.textContent = questions[0].answers[0];
-var answerChoice2 = document.getElementById("button2");
-answerChoice2.textContent = questions[0].answers[1];
-var answerChoice3 = document.getElementById("button3");
-answerChoice3.textContent = questions[0].answers[2];
-var answerChoice4 = document.getElementById("button4");
-answerChoice4.textContent = questions[0].answers[3];
-
-};
-
-
-//add event listener to click and go to next question
-
-function qtn2() {
-    var questionText = document.getElementById("question");
-    questionText.textContent = questions[1].question;
-    var answerChoice1 = document.getElementById("button1");
-    answerChoice1.textContent = questions[1].answers[0];
-    var answerChoice2 = document.getElementById("button2");
-    answerChoice2.textContent = questions[1].answers[1];
-    var answerChoice3 = document.getElementById("button3");
-    answerChoice3.textContent = questions[1].answers[2];
-    var answerChoice4 = document.getElementById("button4");
-    answerChoice4.textContent = questions[1].answers[3];
-    };
-
-
-
-// function quesitonArrayLoop() {
-//     for (var i = 0; i < questions.length; i++) {
-
-//     }
-// } 
-
-
+    for (var i = 0; i < questions.length; i++) {
+        
+        if (
+            (questions[i].answers === false)
+        ) {
+            score--;
+               
+        }
+        else {
+            
+        }
+}};
 
     
-
-// WHEN click start button
-
-// function questionStart( ) {
-//     if (welcomePage.style.display==="none") {
-//         questionContainer.style.display = "block";
-//     }
-//     else (
-//         questionContainer.style.display = "none";
-//     )
-   
-// welcome page hides
-
-// document.getElementById("Welcome-Page").style.display = "none";
-// question1 appears
-// document.getElementById("question-containter").style.display="block";
-
-// stopPropagation ???
-
-// if (document.getElementById("Welcome-Page").style.display = "none";) {
-//     event.stopPropagation;
-// }
-
-// };
-
-
-// function welcomeTextHide() {
-//    var welcomeHide = document.getElementById("Welcome-Page");
-//     welcomeHide.style.display = "none";
-// };
-
-
-// function welcomeOnLoad() {
-//     var welcomePg = document.getElementById("Welcome-Page");
-//     welcomePg.style.visibility = "visible";
-//     var qtnContain = document.getElementById("question-containter");
-//     qtnContain.style.visibility = "none";
-// }
-
-
-// beginBtn.addEventListener("click", qtn1);
-
-// function questionAppear() {
-//     if (welcomeTextHide === true) {
-//         question1.
-//     }
-// }
 
 
 
